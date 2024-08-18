98662f1b8aba4099f1d21937f758ae2ed6148f12
PlatonovSerg <platonov1727@gmail.com>
2024-08-18 13:07:55 +0300

    stylish

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,8 @@
-from django.contrib.postgres.search import (SearchQuery, SearchRank,
-                                            SearchVector, TrigramSimilarity)
+# from django.contrib.postgres.search import (SearchQuery, SearchRank,
+#                                             SearchVector, TrigramSimilarity)
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.db.models import Count
+from django.db.models import Count, Q
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
@@ -161,6 +161,32 @@ def post_comment(request, post_id):
 
 
 # both of search
+# def post_search(request):
+#     form = SearchForm()
+#     query = None
+#     results = []
+
+#     if "query" in request.GET:
+#         form = SearchForm(request.GET)
+#         if form.is_valid():
+#             query = form.cleaned_data["query"]
+#             search_vector = SearchVector("title", "body")
+#             search_query = SearchQuery(query)
+
+
+#             results = (
+#                 Post.published.annotate(
+#                     rank=SearchRank(search_vector, search_query)
+#                     + TrigramSimilarity("title", query)
+#                 )
+#                 .filter(rank__gt=0.1)
+#                 .order_by("-rank")
+#             )
+#     return render(
+#         request,
+#         "blog/post/search.html",
+#         {"form": form, "query": query, "results": results},
+#     )
 def post_search(request):
     form = SearchForm()
     query = None
@@ -170,16 +196,9 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            search_vector = SearchVector("title", "body")
-            search_query = SearchQuery(query)
 
-            results = (
-                Post.published.annotate(
-                    rank=SearchRank(search_vector, search_query)
-                    + TrigramSimilarity("title", query)
-                )
-                .filter(rank__gt=0.1)
-                .order_by("-rank")
+            results = Post.published.filter(
+                Q(title__icontains=query) | Q(body__icontains=query)
             )
     return render(
         request,
