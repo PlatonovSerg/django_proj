5933bc837063d51dabea7e9332230d93ff49e782
PlatonovSerg <platonov1727@gmail.com>
2024-08-26 01:55:30 +0300

    add ru pluralize

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,6 +1,7 @@
 import markdown
 from django import template
 from django.db.models import Count
+from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 from taggit.models import Tag
 
@@ -39,3 +40,28 @@ def markdown_format(text):
             text, extensions=["tables", "fenced_code", "codehilite"]
         )
     )
+
+
+@register.filter(is_safe=False)
+@stringfilter
+def pluralize(value, forms):
+    """
+    Подбирает окончание существительному после числа
+    {{someval|pluralize:"товар,товара,товаров"}}
+    """
+    try:
+        one, two, many = forms.split(",")
+        value = str(value)[-2:]  # 314 -> 14
+
+        if 21 > int(value) > 4:
+            return many
+
+        if value.endswith("1"):
+            return one
+        elif value.endswith(("2", "3", "4")):
+            return two
+        else:
+            return many
+
+    except (ValueError, TypeError):
+        return ""
