8d58c9f64394e0dea6889c12eb5f749c166fd6cd
PlatonovSerg <platonov1727@gmail.com>
2024-08-27 15:15:28 +0300

    fix copy to clipboard button styles

--- mysite/static/js/script.js
+++ mysite/static/js/script.js
@@ -1,31 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('pre').forEach(pre => {
-      // Создаем кнопку "Скопировать"
-      const button = document.createElement('button');
-      button.innerHTML = '<i class="bi bi-clipboard"></i> cкопировать';
-      button.classList.add('copy-btn');
-      button.classList.add('btn-sm');
-      button.classList.add('btn');
-      button.classList.add('d-flex');
-      button.classList.add('float-right');
+    document.querySelectorAll('.codehilite').forEach(container => {
+        // Найти блок <pre> внутри контейнера
+        const pre = container.querySelector('pre');
+        if (!pre) return; // Пропустить, если нет <pre>
+
+        // Создаем кнопку "Скопировать"
+        const button = document.createElement('button');
+        button.innerHTML = '<i class="bi bi-clipboard"></i> Скопировать';
+        button.classList.add('copy-btn');
+        button.classList.add('btn-sm');
+        button.classList.add('btn');
+
+        // Создаем элемент <p> для кнопки и добавляем его перед блоком <pre>
+        const buttonContainer = document.createElement('p');
+        buttonContainer.classList.add('button-container');
+        buttonContainer.appendChild(button);
+
+        // Вставляем <p> с кнопкой перед блоком <pre> внутри контейнера
+        container.insertBefore(buttonContainer, pre);
 
 
-      
-      // Добавляем кнопку перед содержимым блока <pre>
-      pre.style.position = 'relative';
-      pre.prepend(button);
-
-      // Обработчик клика для копирования текста
-      button.addEventListener('click', () => {
-          const code = pre.querySelector('code').innerText;
-          navigator.clipboard.writeText(code).then(() => {
-              button.innerHTML = '<i class="bi bi-clipboard-check"></i> скопированно';
-              setTimeout(() => {
-                  button.innerHTML = '<i class="bi bi-clipboard"></i>';
-              }, 2000);
-          }).catch(err => {
-              console.error('Ошибка копирования: ', err);
-          });
-      });
-  });
+        // Обработчик клика для копирования текста
+        button.addEventListener('click', () => {
+            const code = pre.querySelector('code').innerText;
+            navigator.clipboard.writeText(code).then(() => {
+                button.innerHTML = '<i class="bi bi-clipboard-check"></i> Скопировано';
+                setTimeout(() => {
+                    button.innerHTML = '<i class="bi bi-clipboard"></i> Скопировать';
+                }, 2000);
+            }).catch(err => {
+                console.error('Ошибка копирования: ', err);
+            });
+        });
+    });
 });
